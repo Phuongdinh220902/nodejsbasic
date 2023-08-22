31e226430431344203ee801bf08a2d201aeffa65
Phuongdinh220902 <lnpd0001@gmail.com>
2023-08-22 21:13:18 +0700

    video7

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 // const express = require('express')
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
+require('dotenv').config()
 
 const app = express()
-const port = 2209
+const port = process.env.PORT;
 
+console.log('check: ', port)
 configViewEngine(app);
 
 app.get('/', (req, res) => {
